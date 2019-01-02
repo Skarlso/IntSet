55a5f4487418064dff3be5a63d1de91e6082dafe
Gergely Brautigam <skarlso777@gmail.com>
2019-01-02 22:52:45 +0100

    Added int and rune set.

---
+++ int_set.go
@@ -0,0 +1,100 @@
+package set
+
+// IntSet integer set.
+type IntSet struct {
+	list map[int]struct{} //empty structs occupy 0 memory
+}
+
+// Has returns true if set contains v value.
+func (s *IntSet) Has(v int) bool {
+	_, ok := s.list[v]
+	return ok
+}
+
+// Add adds a value to the set.
+func (s *IntSet) Add(v int) {
+	s.list[v] = struct{}{}
+}
+
+// Remove removes a value from the set.
+func (s *IntSet) Remove(v int) {
+	delete(s.list, v)
+}
+
+// Clear clears the whole list.
+func (s *IntSet) Clear() {
+	s.list = make(map[int]struct{})
+}
+
+// Size returns the size of the set
+func (s *IntSet) Size() int {
+	return len(s.list)
+}
+
+// NewIntSet creates a new integer set.
+func NewIntSet() *IntSet {
+	s := &IntSet{}
+	s.list = make(map[int]struct{})
+	return s
+}
+
+//optional functionalities
+
+//AddMulti Add multiple values in the IntSet.
+func (s *IntSet) AddMulti(list ...int) {
+	for _, v := range list {
+		s.Add(v)
+	}
+}
+
+// IntFilterFunc defines a function that filters out certain values.
+type IntFilterFunc func(v int) bool
+
+// Filter returns a subIntSet, that contains only the values that satisfies the given predicate P
+func (s *IntSet) Filter(P IntFilterFunc) *IntSet {
+	res := NewIntSet()
+	for v := range s.list {
+		if P(v) == false {
+			continue
+		}
+		res.Add(v)
+	}
+	return res
+}
+
+// Union returns the union of the current and another integer set.
+func (s *IntSet) Union(s2 *IntSet) *IntSet {
+	res := NewIntSet()
+	for v := range s.list {
+		res.Add(v)
+	}
+
+	for v := range s2.list {
+		res.Add(v)
+	}
+	return res
+}
+
+// Intersect returns the intersect of the current and another integer set.
+func (s *IntSet) Intersect(s2 *IntSet) *IntSet {
+	res := NewIntSet()
+	for v := range s.list {
+		if s2.Has(v) == false {
+			continue
+		}
+		res.Add(v)
+	}
+	return res
+}
+
+// Difference returns the subIntSet from s, that doesn't exists in s2 (param)
+func (s *IntSet) Difference(s2 *IntSet) *IntSet {
+	res := NewIntSet()
+	for v := range s.list {
+		if s2.Has(v) {
+			continue
+		}
+		res.Add(v)
+	}
+	return res
+}
---
+++ rune_set.go
@@ -0,0 +1,100 @@
+package set
+
+// RuneSet rune set.
+type RuneSet struct {
+	list map[rune]struct{} //empty structs occupy 0 memory
+}
+
+// Has returns true if set contains v value.
+func (s *RuneSet) Has(v rune) bool {
+	_, ok := s.list[v]
+	return ok
+}
+
+// Add adds a value to the set.
+func (s *RuneSet) Add(v rune) {
+	s.list[v] = struct{}{}
+}
+
+// Remove removes a value from the set.
+func (s *RuneSet) Remove(v rune) {
+	delete(s.list, v)
+}
+
+// Clear clears the whole list.
+func (s *RuneSet) Clear() {
+	s.list = make(map[rune]struct{})
+}
+
+// Size returns the size of the set
+func (s *RuneSet) Size() int {
+	return len(s.list)
+}
+
+// NewRuneSet creates a new rune set.
+func NewRuneSet() *RuneSet {
+	s := &RuneSet{}
+	s.list = make(map[rune]struct{})
+	return s
+}
+
+//optional functionalities
+
+//AddMulti Add multiple values in the RuneSet.
+func (s *RuneSet) AddMulti(list ...rune) {
+	for _, v := range list {
+		s.Add(v)
+	}
+}
+
+// RuneFilterFunc defines a function that filters out certain values.
+type RuneFilterFunc func(v rune) bool
+
+// Filter returns a subRuneSet, that contains only the values that satisfies the given predicate P
+func (s *RuneSet) Filter(P RuneFilterFunc) *RuneSet {
+	res := NewRuneSet()
+	for v := range s.list {
+		if P(v) == false {
+			continue
+		}
+		res.Add(v)
+	}
+	return res
+}
+
+// Union returns the union of the current and another rune set.
+func (s *RuneSet) Union(s2 *RuneSet) *RuneSet {
+	res := NewRuneSet()
+	for v := range s.list {
+		res.Add(v)
+	}
+
+	for v := range s2.list {
+		res.Add(v)
+	}
+	return res
+}
+
+// Intersect returns the intersect of the current and another rune set.
+func (s *RuneSet) Intersect(s2 *RuneSet) *RuneSet {
+	res := NewRuneSet()
+	for v := range s.list {
+		if s2.Has(v) == false {
+			continue
+		}
+		res.Add(v)
+	}
+	return res
+}
+
+// Difference returns the sub from s, that doesn't exists in s2 (param)
+func (s *RuneSet) Difference(s2 *RuneSet) *RuneSet {
+	res := NewRuneSet()
+	for v := range s.list {
+		if s2.Has(v) {
+			continue
+		}
+		res.Add(v)
+	}
+	return res
+}
